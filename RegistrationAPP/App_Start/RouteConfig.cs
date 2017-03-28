465b6d5fc3037613034e79f4c3e751403337a289
KazlauskasMantas <kazlauskas.mantas.dev@gmail.com>
2017-03-28 04:03:50 +0300

    Route changed

--- RegistrationAPP/App_Start/RouteConfig.cs
+++ RegistrationAPP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace RegistrationAPP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
